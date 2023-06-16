0e9655ba79607946da1ba8cf6c90e3b4c237897f
Ruben <rubenvegajusto@gmail.com>
2023-06-16 19:13:16 +0100

    implementing reload bottom and three random cat images

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
-const URL = "https://api.thecatapi.com/v1/images/search";
+const API_URL = "https://api.thecatapi.com/v1/images/search?limit=3";
 
-fetch(URL)
-  .then((res) => res.json()) // El metodo fetch me devuelve una promesa y las promesas las podemos resolver con el metodo then. Debo convertir ese then a un objeto de JavaScript. Esto nos devuelve otra promesa
-  .then((data) => {
-    const img = document.querySelector("img"); // Esto significa que agarramos el módulo
-    img.src = data[0].url; // en este caso haremos que la URL aparezca en la etiqueta img y src del HTML
-    // Data ya tiene todo lo que venga de esa API. Si entramos ah’i tenemos un JSON y cada vez que recargamos la p’agina nos da una URL distinta. Para consumir estas APIs desde el navegador, podemos usar la extension de chrome de Google y cada vez que tengamos una respuesta de JSON le dará estilo para que sea legible y lo podamos entender mucho más facilmente
-  });
+async function reload() {
+  const res = await fetch(API_URL); // para indicarle que tiene un llamado asincrono
+  const data = await res.json();
+
+  const img = document.querySelector("img");
+  img.src = data[0].url;
+
+  console.log(data);
+  const img1 = document.getElementById("img1");
+  const img2 = document.getElementById("img2");
+  const img3 = document.getElementById("img3");
+
+  img1.src = data[0].url;
+  img2.src = data[1].url;
+  img3.src = data[2].url;
+}
+
+// Ahora cuando damos click a recargar, nos deberia recargar una imagen. Para que no nos aparezca vacio  anadiremos
+
+reload(); // llama a la funcion reload nada mas carga el código
