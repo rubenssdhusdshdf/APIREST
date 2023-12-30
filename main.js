41c628ffc99d326edb1a0815b08ab6a26e57a108
Ruben <rubenvegajusto@gmail.com>
2023-12-30 20:09:09 +0000

    first css interactions

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=c08d415f-dea7-4a38-bb28-7b2188202e46';
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=4&api_key=c08d415f-dea7-4a38-bb28-7b2188202e46';
 const API_URL_FAVOTITES = 'https://api.thecatapi.com/v1/favourites?api_key=c08d415f-dea7-4a38-bb28-7b2188202e46';
 const API_URL_FAVOTITES_DELETE = (id) => `https://api.thecatapi.com/v1/favourites/${id}?api_key=c08d415f-dea7-4a38-bb28-7b2188202e46`;
 
@@ -15,6 +15,8 @@ async function loadRandomMichis() {
   } else {
     const img1 = document.getElementById('img1');
     const img2 = document.getElementById('img2');
+    const img3 = document.getElementById('img3');
+    const img4 = document.getElementById('img4');
     const btn1 = document.getElementById('btn1');
     const btn2 = document.getElementById('btn2');
     
@@ -27,27 +29,27 @@ async function loadRandomMichis() {
 }
 
 async function loadFavouriteMichis() {
-  const res = await fetch(API_URL_FAVOTITES);
+  const res = await fetch(API_URL_FAVOTITES {
+    method: 'GET',
+    headers: {
+      'X-API-KEY': 'c08d415f-dea7-4a38-bb28-7b2188202e46',
+    }
+  });
   const data = await res.json();
   console.log('Favoritos')
+
   console.log(data)
 
   if (res.status !== 200) {
     spanError.innerHTML = "Hubo un error: " + res.status + data.message;
   } else {
     const section = document.getElementById('favoriteMichis')
-    section.innerHTML = "";
-
-    const h2 = document.createElement('h2');
-    const h2Text = document.createTextNode('Michis favoritos');
-    h2.appendChild(h2Text);
-    section.appendChild(h2);
 
     data.forEach(michi => {
       const article = document.createElement('article');
       const img = document.createElement('img');
       const btn = document.createElement('button');
-      const btnText = document.createTextNode('Sacar al michi de favoritos');
+      const btnText = document.createTextNode('Delete');
 
       img.src = michi.image.url;
       img.width = 150;
